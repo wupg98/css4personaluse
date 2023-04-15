00d571116bc7c0577607579ab757158a16bff88e
wupg98 <106410199+wupg98@users.noreply.github.com>
2023-04-15 05:21:56 +0200

    Create redic.php

---
+++ redic.php
@@ -0,0 +1,14 @@
+<?php
+function Redirect($url, $permanent = false)
+{
+    if (headers_sent() === false)
+    {
+        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+    }
+
+    exit();
+}
+
+Redirect('https://www.openpetition.es/', false);
+die();
+?>
